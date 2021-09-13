9ac7255df6b8bee9a6308579b8cb60ffac633a42
Kris <73711936+krisharring@users.noreply.github.com>
2021-09-13 17:03:14 -0400

    added app.index.js folder

---
+++ app/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+
+const app = express(); 
+
+
+// // error handling - body parser depreceated
+bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// // GET requests
+
+app.get('/', (req, res)=> {
+    res.send('Welcome to My Movie App')
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', {root: _dirname});
+});
+
+app.get('/movies', function(req, res) {
+    res.json(topMovies);
+});
+
+app.get('/secreturl', (req, res) =>{
+    res.send('This is a secret URL');
+});
+
+    // Logging Middleware
+    app.use(express.json());
+    app.use(express.urlencoded({entended: true}));
+    app.use(morgan('common'));
+    // Static Files
+    app.use(express.static('public'));
+    
+// app.use(methodOverride());
+
+let topMovies = [
+    {
+        title: 'Movie One',
+        actor: 'Actor One',
+    },
+    {
+        title: 'Movie Two',
+        actor: 'Actor Two',
+    }, {
+        title: 'Movie Three',
+        actor: 'Actor Three',
+    },
+    {
+        title: 'Movie Four',
+        actor: 'Actor Four',
+    },
+    {
+        title: 'Movie Five',
+        actor: 'Actor Five',
+    },
+    {
+        title: 'Movie Six',
+        actor: 'Actor Six',
+    },
+    {
+        title: 'Movie Seven',
+        actor: 'Actor Seven',
+    },
+    {
+        title: 'Movie Eight',
+        actor: 'Actor Eight',
+    },
+    {
+        title: 'Movie Nine',
+        actor: 'Actor Nine',
+    },
+    {
+        title: 'Movie Ten',
+        actor: 'Actor Ten',
+    },
+];
+
+
+// Error Handler
+app.use((err, req, res, next) => {
+    console.error(err,stack);
+    res.status(500).send('Something Broke');
+});
+
+// Listen to requests
+app.listen(8080, function(req, res){
+    console.log('Movie App is Running on Port 8080...')
+})
